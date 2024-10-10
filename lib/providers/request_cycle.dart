d9cd0b6eac279c06b8ad9cf8de10977e3cba6cbe
yutashimamura <charakuri8@gmail.com>
2024-10-10 16:07:18 +0900

    20241010 commit

--- lib/providers/request_cycle.dart
+++ lib/providers/request_cycle.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:miel_work_request_cycle_web/models/user.dart';
 import 'package:miel_work_request_cycle_web/services/fm.dart';
 import 'package:miel_work_request_cycle_web/services/request_cycle.dart';
 import 'package:miel_work_request_cycle_web/services/user.dart';
@@ -18,10 +17,10 @@ class RequestCycleProvider with ChangeNotifier {
     required String remarks,
   }) async {
     String? error;
-    if (shopName == '') return '会社名は必須入力です';
-    if (shopUserName == '') return '担当者名は必須入力です';
-    if (shopUserEmail == '') return '担当者メールアドレスは必須入力です';
-    if (shopUserTel == '') return '担当者電話番号は必須入力です';
+    if (shopName == '') return '店舗名は必須入力です';
+    if (shopUserName == '') return '使用者名は必須入力です';
+    if (shopUserEmail == '') return '使用者メールアドレスは必須入力です';
+    if (shopUserTel == '') return '使用者電話番号は必須入力です';
     try {
       await FirebaseAuth.instance.signInAnonymously().then((value) {
         String id = _cycleService.id();
@@ -39,17 +38,17 @@ class RequestCycleProvider with ChangeNotifier {
         });
       });
       //通知
-      List<UserModel> sendUsers = [];
-      sendUsers = await _userService.selectList();
-      if (sendUsers.isNotEmpty) {
-        for (UserModel user in sendUsers) {
-          _fmService.send(
-            token: user.token,
-            title: '社外申請',
-            body: '自転車置き場使用の申込がありました',
-          );
-        }
-      }
+      // List<UserModel> sendUsers = [];
+      // sendUsers = await _userService.selectList();
+      // if (sendUsers.isNotEmpty) {
+      //   for (UserModel user in sendUsers) {
+      //     _fmService.send(
+      //       token: user.token,
+      //       title: '社外申請',
+      //       body: '自転車置き場使用の申込がありました',
+      //     );
+      //   }
+      // }
     } catch (e) {
       error = '申込に失敗しました';
     }
